980e13e7a82dfc0fb54fd2c07eeb8e226bcff47d
Mohamed Mohamed <63976154+Mohamed-N-Mohamed@users.noreply.github.com>
2022-09-18 17:46:19 +0100

    completed 8kyu challenge

--- 8kyu/first-none-consecutive-number.js
+++ 8kyu/first-none-consecutive-number.js
@@ -2,9 +2,11 @@
 
 //E.g. If we have an array [1,2,3,4,6,7,8] then 1 then 2 then 3 then 4 are all consecutive but 6 is not, so that's the first non-consecutive number.
 
-for(let i = 0; i < arr.length - 1; i++){
-  if(arr[i] + 1 !== arr[i + 1]){
-    return arr[i + 1]
-  } 
+function firstNonConsecutive (arr) {
+  for(let i = 0; i < arr.length - 1; i++){
+    if(arr[i] + 1 !== arr[i + 1]){
+      return arr[i + 1]
+    } 
+  }
+  return null
 }
-return null
---
+++ wide-mouthed-frog.js
@@ -0,0 +1,6 @@
+//The wide-mouth frog is particularly interested in the eating habits of other creatures
+//
+function mouthSize(animal) {
+  // code here
+  return animal.toLowerCase() === 'alligator' ? 'small' : 'wide'
+}
